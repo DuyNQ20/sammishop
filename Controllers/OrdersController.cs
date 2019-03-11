b5384a1b21121b98c94976129e2bbfd2bee349bd
Nguyen Quang Duy <duynq@tandan.com.vn>
2019-03-11 21:33:55 +0700

    Thêm giao diện và đã view đc giữ liệu từ giỏ hàng

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartPhone.Data;
+using SmartPhone.Lib;
+using SmartPhone.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SmartPhone.Controllers
+{
+    [Route("checkout")]
+    public class OrdersController : Controller
+    {
+        private readonly DataContext _context;
+
+        public OrdersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /<controller>/
+        public async Task<IActionResult> Index()
+        {
+            var dataContext = new List<Cart>();
+            if (HttpContext.Session.GetInt32("CustomerID") != null)
+            {
+                dataContext = await _context.Carts.Include(c => c.Product).Include(c => c.User).Include(c => c.Product.Files).Include(c => c.Product.Supplier).Where(x => x.UserId == HttpContext.Session.GetInt32("CustomerID")).ToListAsync();
+            }
+            else
+            {
+                dataContext = HttpContext.Session.GetObject<List<Cart>>("Carts");
+            }
+            return View(dataContext);
+        }
+    }
+}
